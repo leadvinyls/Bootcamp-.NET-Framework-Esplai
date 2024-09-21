42a13c3379b05204ad94c9c697f1d288339af989
leadvinyls <110293192+LeadVinyls@users.noreply.github.com>
2024-09-21 21:10:59 +0200

    Added calcArea and calcPerimeter

--- ShapesInheritance/Shape.cs
+++ ShapesInheritance/Shape.cs
@@ -9,7 +9,15 @@ namespace ShapesInheritance
 {
     public class Shape
     {
+        public virtual double calcArea() 
+        {
+            return 0;
+        }
 
+        public virtual double calcPerimeter()
+        {
+            return 0;
+        }
     }
 
     public class Poligon : Shape 
@@ -22,9 +30,14 @@ namespace ShapesInheritance
         public double MinorAxis { get; set; }
         public double MajorAxis { get; set; }
 
-        public virtual double calcArea()
+        public override double calcArea()
         {
             return Math.PI * MinorAxis * MajorAxis;
+        }
+
+        public override double calcPerimeter()
+        {
+            return 2 * Math.PI * Math.Sqrt((Math.Pow(MajorAxis, 2) + Math.Pow(MinorAxis, 2)) / 2);
         }
         public Ellipse(double minorAxis, double majorAxis)
         {
@@ -36,22 +49,53 @@ namespace ShapesInheritance
     public class Rectangle : Poligon
     {
         public double Length { get; set; }
-        public double Heigth { get; set; }
+        public double Height { get; set; }
 
-        public virtual double calcArea() 
+        public Rectangle(double length, double height)
         {
-            return Length * Heigth;
+            this.Length = length;
+            this.Height = height;
+            this.NumSides = 4;
+        }
+
+        public override double calcArea() 
+        {
+            return Length * Height;
+        }
+
+        public override double calcPerimeter() 
+        {
+            return (Length + Height) * 2;
         }
 
         public override string ToString() 
         {
-            return $"Length: {Length}, Height: {Heigth}, Area: {calcArea()}";
+            return $"Length: {Length}, Height: {Height}, Area: {calcArea()}";
         } 
-        public Rectangle(double length, double heigth) 
+    }
+
+    public class Triangle : Poligon
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double Angle { get; set; }
+
+        public Triangle(double sideA, double sideB, double angle) 
         {
-            this.Length = length;
-            this.Heigth = heigth;
-            this.NumSides = 4;
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.Angle = angle;
+        }
+
+        public override double calcArea()
+        {
+            return (SideA * SideB * Math.Sin(Angle)) / 2;
+        }
+
+        public override double calcPerimeter()
+        {
+            double sideC = Math.Sqrt(Math.Pow(SideA, 2) + Math.Pow(SideB, 2) - (2 * SideA * SideB * Math.Cos(Angle)));
+            return SideA + SideB + sideC;
         }
     }
 
