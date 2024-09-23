9637e09f92ada9e629fc202e8c03314c8461438d
leadvinyls <110293192+LeadVinyls@users.noreply.github.com>
2024-09-23 09:23:08 +0200

    Shapes inheritance fixed namespace

--- ShapesInheritance/Ellipse.cs
+++ ShapesInheritance/Ellipse.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HerenciaFormas
+namespace ShapesInheritance
 {
     public class Ellipse : Shape
     {
--- ShapesInheritance/Rectangle.cs
+++ ShapesInheritance/Rectangle.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HerenciaFormas
+namespace ShapesInheritance
 {
     public class Rectangle : Poligon
     {
