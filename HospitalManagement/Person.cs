cd0ded3ea00cf8cf30c64452c61e8399e384bf29
leadvinyls <110293192+LeadVinyls@users.noreply.github.com>
2024-09-23 12:58:50 +0200

    Created Hospital Management Project

---
+++ HospitalManagement/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public uint Age { get; set; }
+        public string ID { get; set; }
+
+        public Person() 
+        {
+            Name = "";
+            Age = 0;
+            ID = "00000000X";
+        }
+
+        public Person(string name, uint age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.ID = id;
+        }
+    }
+}
